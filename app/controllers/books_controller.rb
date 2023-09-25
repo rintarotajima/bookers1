d17fc57c40b086a7f6f91cec5a8c19827a49101c
rintarotajima <rintarotajima@gmail.com>
2023-09-25 12:07:39 +0000

    [Add] フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,18 @@ class BooksController < ApplicationController
     #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを作成する。
     @book = Book.new
   end
-  
+
   def create
     #データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice]="Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice]="Book was not successfully created"
+      @books=Book.all
+      render :index
+    end
   end
 
   def index
@@ -22,23 +28,34 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated"
+      redirect_to book_path(@book)
+    else
+      flash.now[:notice]="Updated error"
+      @book=Book.find(params[:id])
+      render :edit
+    end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    if @book.destroy
+      flash[:notice]="Book was successfully deleted"
+    else
+      flash[:alert]="Error deleted"
+    end
+      redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  
+
+
 end
